e98ec66641b10434d02a81370e04a61b61bb243d
Sam Moss <sam.moss321@gmail.com>
2015-11-09 16:42:01 -0600

    initial stable commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -5,44 +5,62 @@
 var message = {};
 var display = [];
 
-$(document).ready(function(){
-
-    refreshMessages();
-
-    $('#messageSubmit').submit(function(event) {
-            event.preventDefault();
-            postMessage($(this))
-        }
-    );
-});
+var messageApp = angular.module('messageApp', []);
 
 
-function postMessage(target) {
-    $.each($(target).serializeArray(), function (i, field) {
-        message[field.name] = field.value;
-    });
-    $.ajax({
-        type:"POST",
-        url:'/message/post',
-        data:message,
-        success: function(data){
-            display.push(data.inputText);
-            refreshMessages();
-        }
-    });
-    $(target).find("input[type=text]").val("");
-}
+messageApp.controller("BodyController",['$scope','$http', function($scope,$http){
+    $scope.message = {};
 
-function refreshMessages(){
-    $('#messages').empty();
-    $.ajax({
-        type:"GET",
-        url:'/message/refresh',
-        success: function(data){
-                    for(var i = 0; i < data.length; i++){
-                        $('#messages').prepend("<div class='message'><p>USER NAME: " + data[i].user_name + "</p>" +
-                            "<p>MESSAGE: " + data[i].message + "</p>");
-                    }
-                }
+    $scope.clickButton = function(input){
+        $http.post('/message/post', input).then(function(response){
+            console.log("This works!");
         })
-}
+    }
+
+}]);
+
+
+
+
+
+//$(document).ready(function(){
+//
+//    refreshMessages();
+//
+//    $('#messageSubmit').submit(function(event) {
+//            event.preventDefault();
+//            postMessage($(this))
+//        }
+//    );
+//});
+//
+//
+//function postMessage(target) {
+//    $.each($(target).serializeArray(), function (i, field) {
+//        message[field.name] = field.value;
+//    });
+//    $.ajax({
+//        type:"POST",
+//        url:'/message/post',
+//        data:message,
+//        success: function(data){
+//            display.push(data.inputText);
+//            refreshMessages();
+//        }
+//    });
+//    $(target).find("input[type=text]").val("");
+//}
+//
+//function refreshMessages(){
+//    $('#messages').empty();
+//    $.ajax({
+//        type:"GET",
+//        url:'/message/refresh',
+//        success: function(data){
+//                    for(var i = 0; i < data.length; i++){
+//                        $('#messages').prepend("<div class='message'><p>USER NAME: " + data[i].user_name + "</p>" +
+//                            "<p>MESSAGE: " + data[i].message + "</p>");
+//                    }
+//                }
+//        })
+//}
